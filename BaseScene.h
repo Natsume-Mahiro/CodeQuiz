bc237e35d62ecb5f588f149b3dacc9d106a140f7
Natsume-Mahiro <natsume.mahiro.0528@gmail.com>
2024-01-13 22:23:36 +0900

    Update BaseScene.h

--- BaseScene.h
+++ BaseScene.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "SceneManager.h"
+
+class BaseScene
+{
+public:
+    BaseScene() : sceneManager(nullptr) {}
+    virtual ~BaseScene() {}
+
+    void setSceneManager(SceneManager& manager) { sceneManager = &manager; }
+
+    virtual void initialize() = 0;
+    virtual void update() = 0;
+    virtual void draw() = 0;
+
+protected:
+    SceneManager* sceneManager;
+};
